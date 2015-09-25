3aea2307beaec98563a9120bf903a4651d1682cc
Ryan Conrad <camalot@gmail.com>
2015-09-24 21:42:41 -0500

    Updated to include the full path to the repo branch to locate the pfx file.

--- .appveyor/appveyor.before-build.ps1
+++ .appveyor/appveyor.before-build.ps1
@@ -1,1 +1,1 @@
-.\.appveyor\InstallPfx.ps1 -pfx "$trunk\Shared\droidexplorer.pfx" -password ((Get-Item Env:\DE_PFX_KEY).Value) -containerName ((Get-Item Env:\VS_PFX_KEY).Value);
+.\.appveyor\InstallPfx.ps1 -pfx "$env:APPVEYOR_BUILD_FOLDER\Shared\droidexplorer.pfx" -password ((Get-Item Env:\DE_PFX_KEY).Value) -containerName ((Get-Item Env:\VS_PFX_KEY).Value);

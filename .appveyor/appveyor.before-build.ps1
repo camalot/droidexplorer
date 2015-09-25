3a2677a3bb854382d8c5c554ecf58a59b81144e4
Ryan Conrad <camalot@gmail.com>
2015-09-24 22:36:22 -0500

    Added the .appveyor path to the PSModulePath

--- .appveyor/appveyor.before-build.ps1
+++ .appveyor/appveyor.before-build.ps1
@@ -1,1 +1,6 @@
 .\.appveyor\InstallPfx.ps1 -pfx "$env:APPVEYOR_BUILD_FOLDER\Shared\droidexplorer.pfx" -password ((Get-Item Env:\DE_PFX_KEY).Value) -containerName ((Get-Item Env:\VS_PFX_KEY).Value);
+
+Write-Host "PSModulePath: $env:PSModulePath";
+# Add the .appveyor folder to the PSModule path
+$env:PSModulePath = "$env:PSModulePath;$env:APPVEYOR_BUILD_FOLDER\.appveyor\";
+Write-Host "PSModulePath: $env:PSModulePath";

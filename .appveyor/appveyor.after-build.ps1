9d77527e9954c74ff25fff339e1b1c766a55c251
Ryan Conrad <camalot@gmail.com>
2015-09-25 22:21:59 -0500

    Set the revision to env:var after build. Write CodePlex release info to a file

--- .appveyor/appveyor.after-build.ps1
+++ .appveyor/appveyor.after-build.ps1
@@ -1,4 +1,6 @@
 if( !(Test-Path -Path Env:\CI_BUILD_VERSION) -and (Test-Path -Path .\VersionAssemblyInfo.txt) ) {
-    $version = (Get-Content -Path .\VersionAssemblyInfo.txt)
+    $version = (Get-Content -Path .\VersionAssemblyInfo.txt);
+		$split = $version.split(".");
     $env:CI_BUILD_VERSION = $version;
+		$env:CI_BUILD_REVISION = $split[3];
 }

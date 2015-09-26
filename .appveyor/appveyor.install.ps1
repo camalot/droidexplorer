188e9c95d7f22ff927ef387c8e4b73ccea08de50
Ryan Conrad <camalot@gmail.com>
2015-09-26 18:42:31 -0500

    make sure cache directory exists.

--- .appveyor/appveyor.install.ps1
+++ .appveyor/appveyor.install.ps1
@@ -6,3 +6,9 @@ choco install -y wixtoolset;
 
 $env:PATH = "C:\ProgramData\chocolatey\lib\ilmerge;C:\ProgramData\chocolatey\lib\NuGet.CommandLine\tools;$env:PATH";
 # set PATH=C:\ProgramData\chocolatey\lib\ilmerge;C:\ProgramData\chocolatey\lib\NuGet.CommandLine\tools;%PATH%
+
+$cache = "$env:APPVEYOR_BUILD_FOLDER\cache\";
+if( !(Test-Path -Path $cache) ) {
+	Write-Host "Creating '$cache'";
+	New-Item -Path $cache -ItemType Directory -Force;
+}

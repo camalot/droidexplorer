d7e2e1537bf0a708902e97ae6ce31e09ed5a0c30
Ryan Conrad <camalot@gmail.com>
2015-09-26 16:03:06 -0500

    Build versions are hard to keep in sync damn it...

--- .appveyor/appveyor.after-build.ps1
+++ .appveyor/appveyor.after-build.ps1
@@ -1,3 +1,5 @@
+
+
 $sourceVersionInfo = "$env:APPVEYOR_BUILD_FOLDER\Shared\VersionAssemblyInfo.txt";
 
 if( (Test-Path -Path $sourceVersionInfo) -and ($env:Platform -eq 'x86') ) {
@@ -8,8 +10,10 @@ if( (Test-Path -Path $sourceVersionInfo) -and ($env:Platform -eq 'x86') ) {
 	$b = $env:APPVEYOR_BUILD_NUMBER;
 	$r = $split[3];
 
-	$env:CI_BUILD_REVISION = $r;
-	Write-Host "Set CI_BUILD_REVISION : $env:CI_BUILD_REVISION";
-	$env:CI_BUILD_VERSION = "$m1.$m2.$b.$r";
-	Write-Host "Set CI_BUILD_VERSION :$env:CI_BUILD_VERSION";
+	Set-AppveyorBuildVariable -Name CI_BUILD_MAJOR -Value $m1;
+	Set-AppveyorBuildVariable -Name CI_BUILD_MINOR -Value $m2;
+
+	Set-AppveyorBuildVariable -Name CI_BUILD_NUMBER -Value $env:APPVEYOR_BUILD_NUMBER;
+	Set-AppveyorBuildVariable -Name CI_BUILD_REVISION -Value $r;
+	Set-AppveyorBuildVariable -Name CI_BUILD_VERSION -Value "$m1.$m2.$b.$r";
 }
--- .appveyor/appveyor.before-build.ps1
+++ .appveyor/appveyor.before-build.ps1
@@ -1,1 +1,22 @@
 .\.appveyor\InstallPfx.ps1 -pfx "$env:APPVEYOR_BUILD_FOLDER\Shared\droidexplorer.pfx" -password ((Get-Item Env:\DE_PFX_KEY).Value) -containerName ((Get-Item Env:\VS_PFX_KEY).Value);
+
+# If it's an x64 build, and the version info file exists.
+# Load those values for the build version
+
+if( $env:Platform -eq "x64" -and (Test-Path -Path $sourceVersionInfo) ) {
+	$sourceVersionInfo = "$env:APPVEYOR_BUILD_FOLDER\Shared\VersionAssemblyInfo.txt";
+  $version = (Get-Content -Path $sourceVersionInfo);
+	$split = $version.split(".");
+	$m1 = $split[0];
+	$m2 = $split[1];
+	$b = $env:APPVEYOR_BUILD_NUMBER;
+	$r = $split[3];
+	
+	Set-AppveyorBuildVariable -Name CI_BUILD_MAJOR -Value $m1;
+	Set-AppveyorBuildVariable -Name CI_BUILD_MINOR -Value $m2;
+
+	Set-AppveyorBuildVariable -Name CI_BUILD_NUMBER -Value $env:APPVEYOR_BUILD_NUMBER;
+	Set-AppveyorBuildVariable -Name CI_BUILD_REVISION -Value $r;
+	Set-AppveyorBuildVariable -Name CI_BUILD_VERSION -Value "$m1.$m2.$b.$r";
+
+}

ec2cb78c27592b793f977f46c573a00f35f45d47
Ryan Conrad <camalot@gmail.com>
2015-09-25 23:11:36 -0500

    running out of ways to set this

--- .appveyor/appveyor.before-deployment.ps1
+++ .appveyor/appveyor.before-deployment.ps1
@@ -16,10 +16,12 @@ $commitMessageRegex = "^\[deploy\:(pre-release|draft|release)\]$";
 	if( Test-Path -Path $env:APPVEYOR_BUILD_FOLDER\Shared\VersionAssemblyInfo.txt ) {
     $version = (Get-Content -Path $env:APPVEYOR_BUILD_FOLDER\Shared\VersionAssemblyInfo.txt);
 		$split = $version.split(".");
-		[Environment]::SetEnvironmentVariable("CI_BUILD_VERSION", $version, "Machine");
+		$env:CI_BUILD_VERSION = $version;
+		#[Environment]::SetEnvironmentVariable("CI_BUILD_VERSION", $version, "Machine");
 		Write-Host "Set CI_BUILD_VERSION : $version";
-		[Environment]::SetEnvironmentVariable("CI_BUILD_REVISION", $split[3], "Machine")
-		Write-Host "Set CI_BUILD_REVISION : $split[3]";
+		#[Environment]::SetEnvironmentVariable("CI_BUILD_REVISION", $split[3], "Machine");
+		$env:CI_BUILD_REVISION = $split[3];
+		Write-Host "Set CI_BUILD_REVISION : ${split[3]}";
 	}
 
 

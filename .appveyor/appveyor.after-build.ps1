500b547c59ec8e21901e738a96ba964c4a9add69
Ryan Conrad <camalot@gmail.com>
2015-09-26 09:31:07 -0500

    cache the version, and use that for x64 started removing some of the old CCNET properties and replace with more generic names like CI_BUILD_VERSION and CI_PROJECT_NAME

--- .appveyor/appveyor.after-build.ps1
+++ .appveyor/appveyor.after-build.ps1
@@ -1,16 +1,25 @@
-	if( Test-Path -Path $env:APPVEYOR_BUILD_FOLDER\Shared\VersionAssemblyInfo.txt ) {
-    $version = (Get-Content -Path $env:APPVEYOR_BUILD_FOLDER\Shared\VersionAssemblyInfo.txt);
-		$split = $version.split(".");
-		$m1 = $split[0];
-		$m2 = $split[1];
-		$b = $env:APPVEYOR_BUILD_NUMBER;
-		$r = $split[3];
-		#[Environment]::SetEnvironmentVariable("CI_BUILD_VERSION", $version, "Machine");
-		#[Environment]::SetEnvironmentVariable("CI_BUILD_REVISION", $split[3], "Machine");
-		$env:CI_BUILD_REVISION = $r;
-		Write-Host "Set CI_BUILD_REVISION : $env:CI_BUILD_REVISION";
-		$env:CI_BUILD_VERSION = "$m1.$m2.$b.$r";
-		Write-Host "Set CI_BUILD_VERSION :$env:CI_BUILD_VERSION";
-	}
+$cachedVersionInfo = "$env:APPVEYOR_BUILD_FOLDER\.cache\VersionAssemblyInfo.cache";
+$sourceVersionInfo = "$env:APPVEYOR_BUILD_FOLDER\Shared\VersionAssemblyInfo.txt";
 
+if( (Test-Path -Path $sourceVersionInfo) -and !(Test-Path -Path $cachedVersionInfo)) {
+	(Get-Content -Path $sourceVersionInfo) | Out-File -FilePath $cachedVersionInfo;
+}
 
+if( Test-Path -Path $cachedVersionInfo ) {
+  $version = (Get-Content -Path $cachedVersionInfo);
+	$split = $version.split(".");
+	$m1 = $split[0];
+	$m2 = $split[1];
+	$b = $env:APPVEYOR_BUILD_NUMBER;
+	$r = $split[3];
+
+	$env:CI_BUILD_REVISION = $r;
+	Write-Host "Set CI_BUILD_REVISION : $env:CI_BUILD_REVISION";
+	$env:CI_BUILD_VERSION = "$m1.$m2.$b.$r";
+	Write-Host "Set CI_BUILD_VERSION :$env:CI_BUILD_VERSION";
+}
+
+# on x64, we can clear this cache file out after we set the env:vars.
+if( (Test-Path -Path Env:\Platform) -and ($env:Platform -eq "x64" ) -and (Test-Path -Path $cachedVersionInfo) ) {
+	(Remove-Item -Path $cachedVersionInfo) | Out-Null;
+}
--- .appveyor/appveyor.before-deployment.ps1
+++ .appveyor/appveyor.before-deployment.ps1
@@ -9,23 +9,8 @@ $commitMessageRegex = "^\[deploy\:(pre-release|draft|release)\]$";
 	# read the publish text file
 	if(Test-Path -Path "$env:APPVEYOR_BUILD_FOLDER\.build\publishchangelog.txt") {
 		$publishNotes = (Get-Content -Path $env:APPVEYOR_BUILD_FOLDER\.build\publishchangelog.txt);
-		[Environment]::SetEnvironmentVariable("CI_RELEASE_DESCRIPTION", $publishNotes, "Machine")
+		$env:CI_RELEASE_DESCRIPTION = $publishNotes;
 		Write-Host "Set CI_RELEASE_DESCRIPTION : $publishNotes";
-	}
-
-	if( Test-Path -Path $env:APPVEYOR_BUILD_FOLDER\Shared\VersionAssemblyInfo.txt ) {
-    $version = (Get-Content -Path $env:APPVEYOR_BUILD_FOLDER\Shared\VersionAssemblyInfo.txt);
-		$split = $version.split(".");
-		$m1 = $split[0];
-		$m2 = $split[1];
-		$b = $env:APPVEYOR_BUILD_NUMBER;
-		$r = $split[3];
-		#[Environment]::SetEnvironmentVariable("CI_BUILD_VERSION", $version, "Machine");
-		#[Environment]::SetEnvironmentVariable("CI_BUILD_REVISION", $split[3], "Machine");
-		$env:CI_BUILD_REVISION = $r;
-		Write-Host "Set CI_BUILD_REVISION : $env:CI_BUILD_REVISION";
-		$env:CI_BUILD_VERSION = "$m1.$m2.$b.$r";
-		Write-Host "Set CI_BUILD_VERSION :$env:CI_BUILD_VERSION";
 	}
 
 # Must come from master branch.

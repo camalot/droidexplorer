15c14ef461741ddc6156cc48996dd1b2b22485a0
Ryan Conrad <camalot@gmail.com>
2015-09-25 23:28:00 -0500

    I dont even understand how this works any more. i think it is time for sleep.

--- .appveyor/appveyor.after-build.ps1
+++ .appveyor/appveyor.after-build.ps1
@@ -1,12 +1,16 @@
 	if( Test-Path -Path $env:APPVEYOR_BUILD_FOLDER\Shared\VersionAssemblyInfo.txt ) {
     $version = (Get-Content -Path $env:APPVEYOR_BUILD_FOLDER\Shared\VersionAssemblyInfo.txt);
 		$split = $version.split(".");
+		$m1 = $split[0];
+		$m2 = $split[1];
+		$b = $env:APPVEYOR_BUILD_NUMBER;
+		$r = $split[3];
 		#[Environment]::SetEnvironmentVariable("CI_BUILD_VERSION", $version, "Machine");
 		#[Environment]::SetEnvironmentVariable("CI_BUILD_REVISION", $split[3], "Machine");
-		$env:CI_BUILD_REVISION = $split[3];
-		Write-Host "Set CI_BUILD_REVISION : ${split[3]}";
-		$env:CI_BUILD_VERSION = "${split[0]}.${split[1]}.$env:APPVEYOR_BUILD_NUMBER.${split[3]}";
-		Write-Host "Set CI_BUILD_VERSION : ${split[0]}.${split[1]}.$env:APPVEYOR_BUILD_NUMBER.${split[3]}";
+		$env:CI_BUILD_REVISION = $r;
+		Write-Host "Set CI_BUILD_REVISION : $env:CI_BUILD_REVISION";
+		$env:CI_BUILD_VERSION = "$m1.$m2.$b.$r";
+		Write-Host "Set CI_BUILD_VERSION :$env:CI_BUILD_VERSION";
 	}
 
 

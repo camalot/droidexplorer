9ad56ae87b99d2a26f7e08461826442a316d76d8
Ryan Conrad <camalot@gmail.com>
2015-09-26 21:34:03 -0500

    [deploy:draft] updated to set the variables needed for doing the Release Publish. created the logic to notify the web server of the new version Added FTP deployment. This is a full deploy test.

--- .appveyor/appveyor.after-deployment.ps1
+++ .appveyor/appveyor.after-deployment.ps1
@@ -1,18 +1,57 @@
 Import-Module "$env:APPVEYOR_BUILD_FOLDER\.appveyor\Invoke-MsBuild.psm1";
 
+function Publish-Release {
+	Params (
+		[Parameter(Mandatory=$true)]
+		[string] $Host
+	)
+	$appId = $env:CI_PUBLISHAPPID;
+	$appKey = $env:CI_PUBLISHKEY;
+
+
+	$url = "http://$host/api/update/create/";
+	$headers = @{};
+	$headers["Authentication-Token"] = $appKey;
+	$headers["Application-Identifier"] = $appId;
+
+	$post = @{
+		Id = $env:CP_RELEASE_ID;
+		Version = [System.Web.HttpUtility]::UrlEncode($env:CI_BUILD_VERSION);
+		Description = [System.Web.HttpUtility]::UrlEncode($env:CI_RELEASE_DESCRIPTION);
+		Name = [System.Web.HttpUtility]::UrlEncode($env:CP_RELEASE_NAME);
+		Url = [System.Web.HttpUtility]::UrlEncode("http://$env:CP_RELEASE_PROJECT.codeplex.com/releases/view/$env:CP_RELEASE_ID");
+	};
+	$contentType = "application/x-www-form-urlencoded";
+	$method = "POST";
+
+	$response = Invoke-WebRequest -Uri $url -Method $method -Headers $headers -TimeoutSec 120 -Body $post -ContentType $contentType -UserAgent "AppVeyor Build Agent";
+
+	return $response;
+}
+
 # trigger the codeplex deployment script
-if( (Test-Path -Path Env:\CI_DEPLOY_CODEPLEX) -and ((Get-Item -Path Env:\CI_DEPLOY_CODEPLEX).Value) -eq $true ) {
+if( $env:CI_DEPLOY_CODEPLEX -eq $true ) {
 	Invoke-MsBuild -Path "$env:APPVEYOR_BUILD_FOLDER\.appveyor\DeployCodePlex.msbuild" -MsBuildParameters "/verbosity:detailed /p:CI_BUILD_VERSION=$env:CI_BUILD_VERSION /p:CI_BUILD_REVISION=$env:CI_BUILD_REVISION /p:CI_BUILD_MAJOR=$env:CI_BUILD_MAJOR /p:CI_BUILD_MINOR=$env:CI_BUILD_MINOR"
 }
 
-if( (Test-Path -Path Env:\CI_DEPLOY_WEBAPI_RELEASE) -and ((Get-Item -Path Env:\CI_DEPLOY_WEBAPI_RELEASE).Value) -eq $true ) {
+# publish release
+if( $env:CI_DEPLOY_WEBAPI_RELEASE -eq $true -and $env:Platform -eq "x86" ) {
+	# this only gets called for the x86 platform so it is called once, and because the env:vars may not exist for x64
+	if( !$env:CP_RELEASE_NAME -or !$env:CP_RELEASE_ID -or !$env:CP_RELEASE_URL ) {
+		Write-Host -BackgroundColor Red -ForegroundColor White "Unable to read the required values to create the release";
+		$host.SetShouldExit(500);
+		return;
+	}
+	#,$env:ProductionApiDomain
+	@($env:DevelopmentApiDomain) | foreach {
+		$host = $_;
+		Write-Host "Publishing Release Information '$env:CP_RELEASE_NAME' to $host";
+		$resp = Publish-Release -Host $host;
+		if($resp.StatusCode -ne 200) {
+			Write-Host -BackgroundColor Red -ForegroundColor White $resp.StatusDescription;
+			$host.SetShouldExit($resp.StatusCode);
+			return;
+		}
 
-}
-
-
-# This cleans up any environment variables that may need to be reset
-# this probably isn't needed, but it doesn't hurt.
-
-if( (Test-Path -Path Env:\CI_BUILD_VERSION) ) {
-    $env:CI_BUILD_VERSION = '';
+	}
 }
--- .appveyor/appveyor.before-deployment.ps1
+++ .appveyor/appveyor.before-deployment.ps1
@@ -5,21 +5,21 @@
 
 $commitMessageRegex = "^\[deploy\:(pre-release|draft|release)\]$";
 
-# THIS SHOULD BE MOVED TO THE MATCH SECTION AFTER TESTING.
-	# read the publish text file
-	if(Test-Path -Path "$env:APPVEYOR_BUILD_FOLDER\.build\publishchangelog.txt") {
-		$publishNotes = (Get-Content -Path $env:APPVEYOR_BUILD_FOLDER\.build\publishchangelog.txt);
-		$env:CI_RELEASE_DESCRIPTION = $publishNotes;
-		Write-Host "Set CI_RELEASE_DESCRIPTION : $publishNotes";
-	}
 
 # Must come from master branch.
 # Must not have a PULL Request Number
 # Must match regex
 if ( !$env:APPVEYOR_PULL_REQUEST_NUMBER -and ($env:APPVEYOR_REPO_BRANCH -eq "master") -and ($env:APPVEYOR_REPO_COMMIT_MESSAGE -match $commitMessageRegex) ) {
-	$env:CI_RELEASE_DESCRIPTION = $env:APPVEYOR_REPO_COMMIT_MESSAGE_EXTENDED
+
+	if(Test-Path -Path "$env:APPVEYOR_BUILD_FOLDER\.build\publishchangelog.txt") {
+		# read the publish text file
+		$publishNotes = (Get-Content -Path $env:APPVEYOR_BUILD_FOLDER\.build\publishchangelog.txt);
+		$env:CI_RELEASE_DESCRIPTION = $publishNotes -replace "\n", "\r\n";
+		Write-Host "Set CI_RELEASE_DESCRIPTION : $publishNotes";
+	}
+
 	$env:CI_DEPLOY_NUGET = $true;
-  $env:CI_DEPLOY_GITHUB = $true;
+  $env:CI_DEPLOY_GITHUB = $false;
   $env:CI_DEPLOY_FTP = $true;
 	$env:CI_DEPLOY_WebHook = $true;
 	$env:CI_DEPLOY_WebDeploy = $true;
@@ -33,5 +33,5 @@ if ( !$env:APPVEYOR_PULL_REQUEST_NUMBER -and ($env:APPVEYOR_REPO_BRANCH -eq "mas
 	$env:CI_DEPLOY_WebHook = $false;
 	$env:CI_DEPLOY_WebDeploy = $false;
 	$env:CI_DEPLOY_CodePlex = $true;
-	$env:CI_DEPLOY_WEBAPI_RELEASE = $false;
+	$env:CI_DEPLOY_WEBAPI_RELEASE = $true;
 }

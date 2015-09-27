b32d030acd77dee7ac3a233e1ab8ffdc32320562
Ryan Conrad <camalot@gmail.com>
2015-09-26 19:54:21 -0500

    Changed the revision to be based the day of the year.

--- .appveyor/appveyor.before-build.ps1
+++ .appveyor/appveyor.before-build.ps1
@@ -1,24 +1,21 @@
 .\.appveyor\InstallPfx.ps1 -pfx "$env:APPVEYOR_BUILD_FOLDER\Shared\droidexplorer.pfx" -password ((Get-Item Env:\DE_PFX_KEY).Value) -containerName ((Get-Item Env:\VS_PFX_KEY).Value);
 
-# If it's an x64 build, and the version info file exists.
-# Load those values for the build version
 
-$sourceVersionInfo = "$env:APPVEYOR_BUILD_FOLDER\cache\AssemblyVersionInfo.txt";
-if( $env:Platform -eq "x64" -and (Test-Path -Path $sourceVersionInfo) ) {
-  $version = (Get-Content -Path $sourceVersionInfo);
-	$split = $version.split(".");
-	$m1 = $split[0];
-	$m2 = $split[1];
-	$b = $env:APPVEYOR_BUILD_NUMBER;
-	$r = $split[3];
-	
-	Set-AppveyorBuildVariable -Name CI_BUILD_MAJOR -Value $m1;
-	Set-AppveyorBuildVariable -Name CI_BUILD_MINOR -Value $m2;
 
-	Set-AppveyorBuildVariable -Name CI_BUILD_NUMBER -Value $env:APPVEYOR_BUILD_NUMBER;
-	Set-AppveyorBuildVariable -Name CI_BUILD_REVISION -Value $r;
-	Set-AppveyorBuildVariable -Name CI_BUILD_VERSION -Value "$m1.$m2.$b.$r";
+Write-Host "Setting up build version";
+$revision = (Get-Date).ToUniversalTime().DayOfYear;
+$version = "$env:APPVEYOR_BUILD_VERSION.$revision";
+$split = $version.split(".");
+$m1 = $split[0];
+$m2 = $split[1];
+$b = $env:APPVEYOR_BUILD_NUMBER;
+$r = $split[3];
 
-	Write-Host "Set the CI_BUILD_VERSION to $env:CI_BUILD_VERSION";
+Set-AppveyorBuildVariable -Name CI_BUILD_MAJOR -Value $m1;
+Set-AppveyorBuildVariable -Name CI_BUILD_MINOR -Value $m2;
 
-}
+Set-AppveyorBuildVariable -Name CI_BUILD_NUMBER -Value $b;
+Set-AppveyorBuildVariable -Name CI_BUILD_REVISION -Value $r;
+Set-AppveyorBuildVariable -Name CI_BUILD_VERSION -Value "$m1.$m2.$b.$r";
+
+Write-Host "Set the CI_BUILD_VERSION to $env:CI_BUILD_VERSION";
--- .appveyor/appveyor.install.ps1
+++ .appveyor/appveyor.install.ps1
@@ -6,10 +6,3 @@ choco install -y wixtoolset;
 
 $env:PATH = "C:\ProgramData\chocolatey\lib\ilmerge;C:\ProgramData\chocolatey\lib\NuGet.CommandLine\tools;$env:PATH";
 # set PATH=C:\ProgramData\chocolatey\lib\ilmerge;C:\ProgramData\chocolatey\lib\NuGet.CommandLine\tools;%PATH%
-
-$cache = "$env:APPVEYOR_BUILD_FOLDER\cache\";
-if( !(Test-Path -Path $cache) ) {
-	Write-Host "Creating '$cache'";
-	New-Item -Path $cache -ItemType Directory -Force | Out-Null;
-	"$env:APPVEYOR_BUILD_VERSION.0" | Out-File -FilePath "$cache\AssemblyVersionInfo.txt";
-}
